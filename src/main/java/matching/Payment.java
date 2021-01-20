3bd3298262f150fb3820aad3795edb3465036ea4
김지연H님/NW Access EP151840C <P151840@sktelecom.com>
2021-01-20 15:53:14 +0900

    개인과제 1st draft

---
+++ src/main/java/matching/Payment.java
@@ -0,0 +1,85 @@
+package matching;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.List;
+
+@Entity
+@Table(name="Payment_table")
+public class Payment {
+
+    @Id
+    private Long matchId;
+    private Integer price;
+    private String paymentAction;
+    private String student; //학생정보 관리를 위한 student 정보 추가
+
+    @PostPersist
+    public void onPostPersist(){
+        PaymentApproved paymentApproved = new PaymentApproved();
+        BeanUtils.copyProperties(this, paymentApproved);
+
+        //바로 이벤트를 보내버리면 주문정보가 커밋되기도 전에 배송발송됨 이벤트가 발송되어 주문테이블의 상태가 바뀌지 않을 수 있다.
+        // TX 리스너는 커밋이 완료된 후에 이벤트를 발생하도록 만들어준다.
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+            @Override
+            public void beforeCommit(boolean readOnly) {
+                paymentApproved.publish();
+            }
+        });
+        try {
+            //istio test를 위해 payment sleep 추가가
+           Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        PaymentCanceled paymentCanceled = new PaymentCanceled();
+        BeanUtils.copyProperties(this, paymentCanceled);
+        paymentCanceled.publishAfterCommit();
+
+
+    }
+
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    public String getPaymentAction() {
+        return paymentAction;
+    }
+
+    public void setPaymentAction(String paymentAction) {
+        this.paymentAction = paymentAction;
+    }
+
+    /** student 정보 추가 **/
+    public String getStudent() {
+        return student;
+    }
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+
+
+
+}
